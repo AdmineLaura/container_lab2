daddc3a7423b7fc0029d545efda194032a84f9e9
Admine <l.silinyte@gmail.com>
2025-01-07 09:39:42 +0200

    Update check_bgp_status.sh

--- check_bgp_status.sh
+++ check_bgp_status.sh
@@ -3,4 +3,4 @@
 docker exec clab-network-homework-spine01  vtysh -c "show bgp summary"
 docker exec clab-network-homework-leaf01  vtysh -c "show bgp summary"
 docker exec clab-network-homework-leaf02  vtysh -c "show bgp summary"
-
+docker exec clab-network-homework-leaf03  vtysh -c "show bgp summary"
